425ec8958d583a07e8911edab58ec5ccf58af0d6
lifechoices <sithandathuzipho@gmail.com>
2021-07-02 16:04:21 +0200

    finished pokedex

--- index.js
+++ index.js
@@ -3,28 +3,22 @@ let base_URL = "https://pokeapi.co/api/v2/pokemon/";
 // Function to fetch a list of pokemon
 function getPokemonList(url) {
   fetch(url)
-    // Convert data from JSON
     .then((response) => response.json())
-    //Stuff to do with data
     .then((data) => {
-      // Console log to make sure I am getting the data
       console.log(data);
       // Get the list of pokemon from the results
       let pokemon = data.results;
       // Get element from HTML to write buttons in
       let container = document.querySelector(".pokemon-list-container");
-      // Clear the container
       container.innerHTML = "";
-      // Loop over pokemon list and create an HTML button for each one. Add the button to the container
       pokemon.forEach((btn) => {
         container.innerHTML += `<button onclick="getPokemonInfo('${btn.url}')">${btn.name}</button>`;
       });
-      // Add a next pokemon button
+      // next pokemon button
       container.innerHTML += `<br><br><button onclick="getPokemonList('${data.next}')">Next</button>`;
-          // Previous pokemon button
-      container.innerHTML += `<br><br><button onclick="getPokemonList('${data.previous}')">Previous</button>`; 
+      // Previous pokemon button
+      container.innerHTML += `<br><br><button onclick="getPokemonList('${data.previous}')">Previous</button>`;
     });
-
 }
 
 // Get default pokemon list
@@ -35,11 +29,17 @@ function getPokemonInfo(url) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      // Make sure data comes throufg
       console.log(data);
-      // Write data to pokemon information container
       document.querySelector(".pokemon-info").innerHTML = `
-    <img src="${data.sprites.front_default} ">
+    <img src="${data.sprites.other.dream_world.front_default} ">
     `;
+      document.querySelector(
+        ".pokemon-info"
+      ).innerHTML += `<p>Name: ${data.name}</p>`;
+      document.querySelector(".pokemon-info").innerHTML += `<p>Type: ${data.types[0].type.name}</p>`;
+      document.querySelector(
+        ".pokemon-info"
+      ).innerHTML += `<p>Weight: ${data.weight}</p>`;
+      document.querySelector(".pokemon-info").innerHTML += `<p>Height: ${data.height}</p>`;
     });
 }
